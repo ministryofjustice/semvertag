490b6b23f6800579288d80e1500e52aaf0603371
Mateusz Łapsa-Malawski <m@cr3.io>
2015-04-13 17:41:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='semvertag',
-    version='0.0.1',
+    version='0.0.2',
     url='http://github.com/ministryofjustice/semvertag',
     license='MIT',
     author='',
